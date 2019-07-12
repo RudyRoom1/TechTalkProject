112a7bc65bd1f869d0d0ff14ac20de439b269508
yaroslav_kush <yaroslav_kush@epam.com>
2019-07-12 14:33:04 +0300

    Second part of VERY useful feature)

--- src/main/java/app/servlets/AddServlet.java
+++ src/main/java/app/servlets/AddServlet.java
@@ -24,6 +24,7 @@ public class AddServlet extends HttpServlet {
         String name = req.getParameter("name");
         String name = req.getParameter("name");
         String password = req.getParameter("password");
+        String password = req.getParameter("password");
         User user = new User(name, password);
         Model model = Model.getInstance();
         model.add(user);
