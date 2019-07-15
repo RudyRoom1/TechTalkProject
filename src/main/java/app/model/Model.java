ef791e8730957c20c2ec03213aad9634fd5d9d70
yaroslav_kush <yaroslav_kush@epam.com>
2019-07-15 13:26:28 +0300

    First part of useful feature in BadBranch

--- src/main/java/app/model/Model.java
+++ src/main/java/app/model/Model.java
@@ -8,10 +8,9 @@ import java.util.stream.Collectors;
 
 public class Model {
     private static Model instance = new Model();
+    private static Model instance1 = new Model();
 
     private List<User> model;
-    private List<User> model1;
-
 
     public static Model getInstance() {
         return instance;
